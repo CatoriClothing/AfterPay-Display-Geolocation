e74be1d0a9c7ae563169577f010320069f659e74
CatoriClothing <76810766+CatoriClothing@users.noreply.github.com>
2021-01-24 02:38:27 +1100

    Create AfterPay-Geoloc.js

---
+++ AfterPay-Geoloc.js
@@ -0,0 +1,10 @@
+<script>
+document.addEventListener('page:loaded', function() {
+$.get("https://ipinfo.io/json", function (response) {
+  if (response.country == "US" ){
+$.getScript("https://static.afterpay.com/shopify-afterpay-javascript.js", function(data, textStatus, jqxhr) {
+});
+  }
+},"jsonp");
+});
+</script>
