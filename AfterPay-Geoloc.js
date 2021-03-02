3343a56b6db9f01df6b04c1c67df16adb35f1a18
CatoriClothing <76810766+CatoriClothing@users.noreply.github.com>
2021-03-02 16:12:08 +1100

    Update AfterPay-Geoloc.js

--- AfterPay-Geoloc.js
+++ AfterPay-Geoloc.js
@@ -1,5 +1,6 @@
 <script>
 document.addEventListener('page:loaded', function() {
+$.ajaxSetup({xhrFields: { withCredentials: true } }); // Pass through to allow for basic rate limiting
 $.get("https://ipinfo.io/json", function (response) {
   if (response.country == "US" ){
 $.getScript("https://static.afterpay.com/shopify-afterpay-javascript.js", function(data, textStatus, jqxhr) {
